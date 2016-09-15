a5ad7373cac4d70519509d7245b2278d681c0d64
WasssHere <washasmail@gmail.com>
2016-09-14 22:46:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+function save() {
+	var save = {
+		pandas: pandas,
+		babypanda: babypanda,
+		teenagepanda: teenagepanda
+	}
+	localStorage.setItem("save",JSON.stringify(save));
+}
+
+function load() {
+	var savegame = JSON.parse(localStorage.getItem("save"));
+	if (typeof savegame.pandas !== "undefined") pandas = savegame.pandas;
+	if (typeof savegame.babypanda !== "undefined") babypanda = savegame.babypanda;
+	if (typeof savegame.teenagepanda !== "undefined") teenagepanda = savegame.teenagepanda;
+}
+
+function reset() {
+	localStorage.removeItem("save")
+}
+
+function openAlertSave() {
+	alert("Game Saved.")
+}
+
+function openAlertLoad() {
+	alert("Game Loaded.")
+}
+
+function openAlertReset() {
+	alert("Game Reset.")
+}
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
@@ -33,6 +65,26 @@ function buyBabyPanda(number){
     document.getElementById('babypandacost').innerHTML = nextCost;  
 };
 
+var teenagepanda = 0;
+function buyTeenagePanda(number){
+    var teenagepandacost = Math.floor(100 * Math.pow(1.1,teenagepanda));     
+    if(pandas >= teenagepandacost){                                  
+        teenagepanda = teenagepanda + 1;                                   
+        pandas = pandas - teenagepandacost;                          
+        document.getElementById('teenagepanda').innerHTML = teenagepanda;  
+        document.getElementById('pandas').innerHTML = pandas;  
+    };
+    var nextCost = Math.floor(100 * Math.pow(1.1,teenagepanda));      
+    document.getElementById('teenagepandacost').innerHTML = nextCost;  
+};
+
+
 window.setInterval(function(){
 onePanda(babypanda);
 }, 1000);
+
+window.setInterval(function(){
+onePanda(teenagepanda * 10);
+}, 1000);
+
+
